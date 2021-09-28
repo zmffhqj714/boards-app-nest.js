78ec27befaf867d1cd12d1c61ffd2972b2c54d52
Bon <zmffhqj714@naver.com>
2021-09-28 22:53:18 +0900

    ser

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private boardsService:BoardsService) {
-    this.boardsService.getBoardsService()
-}
-
-
-}
+    constructor(private boardsService: BoardsService) {}
+    
+    @Get()
+    getAllboards():object{
+        return this.boardsService.getAllboards();
+    }
+} 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()//생성자 종류를 다른 컴포넌트 에서 사용가능하도록 하는 문법
 export class BoardsService {
-    getAllBoards() {
-        return {
-        
-        }
+    private boards = []; //임의데이터메모리, 이거 왜ㅜ변수타입 지정안해주냐ㅕ?
+
+
+
+    getAllboards() {
+        return this.boards;  //폴더보드는 따로 불러올 수 있나?
     }
 }
